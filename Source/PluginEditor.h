0d4c420d0d2ecac71d4cd6a7c63cda59fa75dd2e
Craig Olson <craig.olson804@gmail.com>
2023-03-29 09:45:38 -0700

    draw response area grid lines

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -66,6 +66,8 @@ juce::Timer
     
     void paint (juce::Graphics& g) override;
     
+    void resized() override;
+    
 private:
     SimpleEQAudioProcessor& audioProcessor;
     juce::Atomic<bool> parametersChanged { false };
@@ -73,6 +75,12 @@ private:
     MonoChain monoChain;
     
     void updateChain();
+    
+    juce::Image background;
+    
+    juce::Rectangle<int> getRenderArea();
+    
+    juce::Rectangle<int> getAnalysisArea();
 };
 
 //==============================================================================
