3d9023adb25afde58ccc938973bbb33eb8b9af37
Craig Olson <craig.olson804@gmail.com>
2023-04-01 09:57:30 -0700

    Customized graphics of bypass button

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -139,9 +139,15 @@ private:
 
 struct LookAndFeel : juce::LookAndFeel_V4
 {
-    virtual void drawRotarySlider (juce::Graphics&, int x, int y, int width,
-                                   int height, float sliderPosProportional,
-                                   float rotaryStartAngle, float rotaryEndAngle, juce::Slider& slider) override;
+    void drawRotarySlider (juce::Graphics&, int x, int y, int width,
+                          int height, float sliderPosProportional,
+                          float rotaryStartAngle, float rotaryEndAngle,
+                          juce::Slider& slider) override;
+    
+    void drawToggleButton (juce::Graphics& g,
+                           juce::ToggleButton& toggleButton,
+                           bool shouldDrawButtonAsHighlighted,
+                           bool shouldDrawButtonAsDown) override;
 };
 
 struct RotarySliderWithLabels : juce::Slider
@@ -282,6 +288,8 @@ private:
     analyzerEnabledButtonAttachment;
     
     std::vector<juce::Component*> getComps();
+    
+    LookAndFeel lnf;
     
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
